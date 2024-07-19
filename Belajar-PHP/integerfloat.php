7e9dda8d33e47ea3acc88952fc50d671c669746f
Retiishia <azizfarhan72@gmail.com>
2024-07-19 20:18:36 +0700

    Speedrunning Learning PHP AND CI

---
+++ Belajar-PHP/integerfloat.php
@@ -0,0 +1,17 @@
+<?php
+$a=10;
+$a++;
+
+echo $a; // menghasilkan nilai 11
+
+$b = 10;
+$b--;
+
+echo $b; //menghasilkan nilai 9
+
+$c = 10;
+$c++;
+$c++;
+
+echo $c;//akan menghasilkan nilai 12 karena menggunakan 2x operator increment
+?>
