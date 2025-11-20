33d0e8ba2890bd1e1bc611980c557eab7207d2d3
Retiishia <azizfarhan72@gmail.com>
2025-11-21 06:47:00 +0700

    feat: learning a Components Feature

---
+++ Learn-ReactJS/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Article from './components/Article.jsx'
+
+function App() {
+
+  return (
+      <div className="App">
+          <Article />
+          <Article />
+        </div>
+  )
+}
+
+export default App
---
+++ Learn-ReactJS/vite-project/src/components/Article.jsx
@@ -0,0 +1,5 @@
+function Article() {
+    return <div>Ini adalah component pertama, dan kedua</div>
+}
+
+export default Article
