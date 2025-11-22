c60e874696a1d0f77a1f6c7899e7ea8fb0e9c0a2
Retiishia <azizfarhan72@gmail.com>
2025-11-23 05:01:55 +0700

    feat: added jsx learn

--- Learn-ReactJS/vite-project/src/components/Article.jsx
+++ Learn-ReactJS/vite-project/src/components/Article.jsx
@@ -1,6 +1,22 @@
 function Article() {
     const name = "Farhan Aziz"
-    return <div>{name}</div>
-}
+    const titles = ["React", "Vue", "Angular"]
 
+    return (
+    <>
+        <h1>Article Component</h1>
+        <div>{name}</div>
+        <div>
+            {titles.map((title) => {
+                return (
+                <>
+                <div>{title}</div>
+                <div>{title}</div>
+                </>
+            );
+            })}
+            </div>
+    </>
+    );
+}
 export default Article
